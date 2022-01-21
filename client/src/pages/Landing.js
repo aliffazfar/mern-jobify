7d98a55e21d45504e76b36d54b1149e29356c7b4
aliff azfar <aliffazfar34@gmail.com>
2022-01-21 23:15:22 +0800

    link react-router-dom

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import { Logo } from '../components'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -20,7 +21,9 @@ const Landing = () => {
             messenger bag glossier. Pitchfork pug aesthetic retro, snackwave
             food truck fingerstache sriracha actually selvage beard.
           </p>
-          <button className='btn btn-hero'>Login/Register</button>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
       </div>
