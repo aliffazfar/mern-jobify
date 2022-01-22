c1e966c072c7faea5098fa3c74f4ba000dec0b4d
aliff azfar <aliffazfar34@gmail.com>
2022-01-23 02:07:30 +0800

    errorhandler

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ import express from 'express'
 const app = express()
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js '
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req, res) => {
+  throw new Error('error')
   res.send('Welcome!')
 })
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 4444
 
