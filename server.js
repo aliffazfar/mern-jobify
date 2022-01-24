38dee50d4240d5b6491a94765938f216e6dd4d0b
aliff azfar <aliffazfar34@gmail.com>
2022-01-24 16:23:55 +0800

    env mongodb

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+import connectDB from './db/connect.js'
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
@@ -17,4 +19,15 @@ app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 4444
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
