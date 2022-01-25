e9fe3176401f0ddb288ce397ae6b4346f05c7421
aliff azfar <aliffazfar34@gmail.com>
2022-01-25 22:47:06 +0800

    http-status-codes

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,12 @@
+import { StatusCodes } from 'http-status-codes'
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err)
-  res.status(500).json({ msg: err })
+  const defaultError = {
+    StatusCoded: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: 'Something went wrong, try again later',
+  }
+  res.status(defaultError.StatusCode).json({ msg: err })
 }
 
 export default errorHandlerMiddleware
