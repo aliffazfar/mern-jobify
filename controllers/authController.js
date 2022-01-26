4ebee16737a763d3c8479698e571955600ce1b82
aliff azfar <aliffazfar34@gmail.com>
2022-01-26 11:28:51 +0800

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,8 +14,17 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password })
-  user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  })
 }
 
 const login = async (req, res) => {
