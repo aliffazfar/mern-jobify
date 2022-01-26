4541819332746f805667b1386103fe4d1cf2a4f4
aliff azfar <aliffazfar34@gmail.com>
2022-01-26 11:41:43 +0800

    concurrently

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,7 @@
 import Error from './Error'
 
 const Dashboard = () => {
-  return (
-    <h1>
-      <Error />
-      Dashboard
-    </h1>
-  )
+  return <h1>Dashboard </h1>
 }
 
 export default Dashboard
