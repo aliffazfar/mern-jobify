985cf4c9e1f15e7a549b209fe65bbf3c60bf187d
aliff azfar <aliffazfar34@gmail.com>
2022-01-30 01:23:23 +0800

    frontend-backend

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,7 +1,7 @@
 import React, { useReducer, useContext } from 'react'
 
 import reducer from './reducer'
-
+import axios from 'axios'
 import {
   DISPLAY_ALERT,
   CLEAR_ALERT,
@@ -18,6 +18,7 @@ const initialState = {
   user: null,
   token: null,
   userLocation: '',
+  jobLocation: '',
 }
 
 const AppContext = React.createContext()
@@ -37,7 +38,24 @@ const AppProvider = ({ children }) => {
   }
 
   const registerUser = async (currentUser) => {
-    console.log(currentUser)
+    dispatch({ type: REGISTER_USER_BEGIN })
+    try {
+      const response = await axios.post('/api/v1/auth/register', currentUser)
+      console.log(response)
+      const { user, token, location } = response.data
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: { user, token, location },
+      })
+      // local storage later
+    } catch (error) {
+      console.log(error.response)
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      })
+    }
+    clearAlert()
   }
 
   return (
