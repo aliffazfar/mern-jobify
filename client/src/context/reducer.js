0f70eea5c3ad4281a47fe57371a27bd52808d269
aliff azfar <aliffazfar34@gmail.com>
2022-02-08 02:14:50 +0800

    refactor

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -7,6 +7,9 @@ import {
   LOGIN_USER_BEGIN,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
 } from './actions'
 
 const reducer = (state, action) => {
@@ -82,7 +85,31 @@ const reducer = (state, action) => {
       alertText: action.payload.msg,
     }
   }
-
+  if (action.type === SETUP_USER_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+  if (action.type === SETUP_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: action.payload.alertText,
+    }
+  }
+  if (action.type === SETUP_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    }
+  }
   throw new Error(`no such action :${action.type}`)
 }
 export default reducer
