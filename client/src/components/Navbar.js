460751afa2defcc1e9823ed25da8b17065c79cc7
aliff azfar <aliffazfar34@gmail.com>
2022-02-08 15:11:40 +0800

    add structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import { useState } from 'react'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 import Wrapper from '../assets/wrappers/Navbar'
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>navbar</h4>
+      <div className='nav-center'>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => console.log('show/hide dropdown')}
+          >
+            <FaUserCircle />
+            John
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
