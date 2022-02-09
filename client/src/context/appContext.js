ecdf407a471671ba9fa23362ecea2b1ecca43f04
aliff azfar <aliffazfar34@gmail.com>
2022-02-09 18:14:32 +0800

    logout token

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -58,9 +58,9 @@ const AppProvider = ({ children }) => {
       return response
     },
     (error) => {
-      console.log(error.response)
+      // console.log(error.response)
       if (error.response.status === 401) {
-        console.log('AUTH ERROR')
+        logoutUser()
       }
       return Promise.reject(error)
     }
@@ -131,10 +131,12 @@ const AppProvider = ({ children }) => {
 
       addUserToLocalStorage({ user, location, token })
     } catch (error) {
-      dispatch({
-        type: UPDATE_USER_ERROR,
-        payload: { msg: error.response.data.msg },
-      })
+      if (error.response.status !== 401) {
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: error.response.data.msg },
+        })
+      }
     }
     clearAlert()
   }
--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -13,11 +13,11 @@ const Profile = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    // if (!name || !email || !lastName || !location) {
-    //   // test and remove temporary
-    //   displayAlert()
-    //   return
-    // }
+    if (!name || !email || !lastName || !location) {
+      // test and remove temporary
+      displayAlert()
+      return
+    }
 
     updateUser({ name, email, lastName, location })
   }
