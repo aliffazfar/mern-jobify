f9f8297a75079daf0fe87e14dedd6f32209d5a45
aliff azfar <aliffazfar34@gmail.com>
2022-02-10 00:22:38 +0800

    global handle

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -13,6 +13,7 @@ import {
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
   UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -149,6 +150,13 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const handleChange = ({ name, value }) => {
+    dispatch({
+      type: HANDLE_CHANGE,
+      payload: { name, value },
+    })
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -158,6 +166,7 @@ const AppProvider = ({ children }) => {
         toggleSidebar,
         logoutUser,
         updateUser,
+        handleChange,
       }}
     >
       {children}
