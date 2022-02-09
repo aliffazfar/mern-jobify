a47184bc24579ffbaf2aa42fab830821e1978e9d
aliff azfar <aliffazfar34@gmail.com>
2022-02-09 17:50:22 +0800

    interceptors

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -34,7 +34,34 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   //axios
-  axios.defaults.headers.common['Authorization'] = `Bearer ${state.token}`
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+  })
+
+  // request interceptor
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+
+  // response interceptor
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response
+    },
+    (error) => {
+      console.log(error.response)
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR')
+      }
+      return Promise.reject(error)
+    }
+  )
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT })
@@ -87,10 +114,9 @@ const AppProvider = ({ children }) => {
   }
   const updateUser = async (currentUser) => {
     try {
-      const { data } = await axios.patch('/api/v1/auth/updateUser', currentUser)
-      console.log(data)
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser)
     } catch (error) {
-      console.log(error.response)
+      // console.log(error.response)
     }
   }
 
