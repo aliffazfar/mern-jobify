a9311d48b72e3c785ea241940f96e1c7e8405851
aliff azfar <aliffazfar34@gmail.com>
2022-02-10 21:50:18 +0800

    get jobs

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -18,6 +18,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -42,6 +44,10 @@ const initialState = {
   jobType: 'full-time',
   statusOptions: ['pending', 'interview', 'declined'],
   status: 'pending',
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
 }
 
 const AppContext = React.createContext()
@@ -190,6 +196,27 @@ const AppProvider = ({ children }) => {
     }
     clearAlert()
   }
+  const getJobs = async () => {
+    let url = `/jobs`
+
+    dispatch({ type: GET_JOBS_BEGIN })
+    try {
+      const { data } = await authFetch(url)
+      const { jobs, totalJobs, numOfPages } = data
+      dispatch({
+        type: GET_JOBS_SUCCESS,
+        payload: {
+          jobs,
+          totalJobs,
+          numOfPages,
+        },
+      })
+    } catch (error) {
+      console.log(error.response)
+      logoutUser()
+    }
+    clearAlert()
+  }
 
   return (
     <AppContext.Provider
@@ -203,6 +230,7 @@ const AppProvider = ({ children }) => {
         handleChange,
         clearValues,
         createJob,
+        getJobs,
       }}
     >
       {children}
