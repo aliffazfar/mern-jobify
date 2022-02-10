735be48fb6b9c3c6443988d9184844f34b765d7b
aliff azfar <aliffazfar34@gmail.com>
2022-02-11 02:29:16 +0800

    stats

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -25,6 +25,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -53,6 +55,8 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats: {},
+  monthlyApplications: [],
 }
 
 const AppContext = React.createContext()
@@ -259,6 +263,24 @@ const AppProvider = ({ children }) => {
     }
     clearAlert()
   }
+  const showStats = async () => {
+    dispatch({ type: SHOW_STATS_BEGIN })
+    try {
+      const { data } = await authFetch('/jobs/stats')
+      dispatch({
+        type: SHOW_STATS_SUCCESS,
+        payload: {
+          stats: data.defaultStats,
+          monthlyApplications: data.monthlyApplications,
+        },
+      })
+    } catch (error) {
+      console.log(error.response)
+      // logoutUser()
+    }
+
+    clearAlert()
+  }
 
   return (
     <AppContext.Provider
@@ -276,6 +298,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
       }}
     >
       {children}
