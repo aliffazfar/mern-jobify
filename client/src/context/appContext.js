42dc48a634940387261f01b74310a599fc611374
aliff azfar <aliffazfar34@gmail.com>
2022-02-11 01:44:37 +0800

    frontend del

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -21,6 +21,7 @@ import {
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
+  DELETE_JOB_BEGIN,
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -221,8 +222,14 @@ const AppProvider = ({ children }) => {
   const setEditJob = (id) => {
     dispatch({ type: SET_EDIT_JOB, payload: { id } })
   }
-  const deleteJob = (id) => {
-    console.log(`delete : ${id}`)
+  const deleteJob = async (jobId) => {
+    dispatch({ type: DELETE_JOB_BEGIN })
+    try {
+      await authFetch.delete(`/jobs/${jobId}`)
+      getJobs()
+    } catch (error) {
+      logoutUser()
+    }
   }
   const editJob = () => {
     console.log('edit job')
