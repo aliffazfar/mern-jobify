1ea2abe1a959b7ad5dab57daded064e372e55e9f
aliff azfar <aliffazfar34@gmail.com>
2022-02-11 01:12:37 +0800

    backend edit

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,10 @@
 import Job from '../models/Job.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError, NotFoundError } from '../errors/index.js'
+import {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+} from '../errors/index.js'
 
 const createJob = async (req, res) => {
   const { position, company } = req.body
@@ -24,7 +28,28 @@ const getAllJobs = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-  res.send('update job')
+  const { id: jobId } = req.params
+
+  const { company, position } = req.body
+
+  if (!company || !position) {
+    throw new BadRequestError('Please Provide All Values')
+  }
+
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  // check permissions
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(StatusCodes.OK).json({ updatedJob })
 }
 
 const deleteJob = async (req, res) => {
