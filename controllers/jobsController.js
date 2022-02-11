ccce95b97829b663ba6f2080bb40e5451b6fba8e
aliff azfar <aliffazfar34@gmail.com>
2022-02-12 01:21:17 +0800

    getall

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -23,7 +23,18 @@ const createJob = async (req, res) => {
 }
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId })
+  const { search, status, jobType, sort } = req.query
+
+  const queryObject = {
+    createdBy: req.user.userId,
+  }
+
+  // NO AWAIT
+  let result = Job.find(queryObject)
+
+  // chain sort conditions
+
+  const jobs = await result
 
   res
     .status(StatusCodes.OK)
