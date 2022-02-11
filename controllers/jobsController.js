9af1e22afecf62f09f7879bef7f51e79d091cc3f
aliff azfar <aliffazfar34@gmail.com>
2022-02-12 01:30:19 +0800

    regex search

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -35,11 +35,25 @@ const getAllJobs = async (req, res) => {
   if (jobType !== 'all') {
     queryObject.jobType = jobType
   }
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' }
+  }
   // NO AWAIT
   let result = Job.find(queryObject)
 
   // chain sort conditions
-
+  if (sort === 'latest') {
+    result = result.sort('-createdAt')
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position')
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position')
+  }
   const jobs = await result
 
   res
