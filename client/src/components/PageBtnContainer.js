9d4d4d3e5f93107bb350f4ab791ba3052c713900
aliff azfar <aliffazfar34@gmail.com>
2022-02-12 02:32:31 +0800

    btncss

---
+++ client/src/components/PageBtnContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageBtnContainer = () => {
+  return (
+    <div>
+      <h2> Page btn container</h2>
+    </div>
+  )
+}
+
+export default PageBtnContainer
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -54,14 +54,16 @@ const getAllJobs = async (req, res) => {
   // setup pagination
   const page = Number(req.query.page) || 1
   const limit = Number(req.query.limit) || 10
-  const skip = (page - 1) * limit //10
+  const skip = (page - 1) * limit
+
   result = result.skip(skip).limit(limit)
-  // 75
-  // 10 10 10 10 10 10 10 5
+
   const jobs = await result
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages })
 }
 
 const updateJob = async (req, res) => {
