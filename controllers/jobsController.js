79e9999d03e6a2eb530876c75836bc7d86ab6f51
aliff azfar <aliffazfar34@gmail.com>
2022-02-12 02:22:52 +0800

    limit

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -51,15 +51,13 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position')
   }
 
-  const totalJobs = await result
-
   // setup pagination
-  const limit = 10
-  const skip = 1
-
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit //10
   result = result.skip(skip).limit(limit)
-  // 23
-  // 4 7 7 7 2
+  // 75
+  // 10 10 10 10 10 10 10 5
   const jobs = await result
   res
     .status(StatusCodes.OK)
