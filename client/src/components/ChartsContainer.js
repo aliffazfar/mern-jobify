bb1f47782afd550b0767a3ece9b76202e6ebf520
aliff azfar <aliffazfar34@gmail.com>
2022-02-12 00:32:49 +0800

    stats frontend

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ChartsContainer = () => {
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+
+export default function ChartsContainer() {
+  const [barChart, setBarChart] = useState(true)
+  const { monthlyApplications: data } = useAppContext()
+
   return (
-    <div>
-      <h1>Chart Container</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'AreaChart' : 'BarChart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
-
-export default ChartsContainer
