de2ff4f7044586ccae3d8b69f232ac302750cc49
aliff azfar <aliffazfar34@gmail.com>
2022-02-15 00:03:40 +0800

    route limiter

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
 // db and authenticateUser
 import connectDB from './db/connect.js'
 
@@ -27,6 +31,11 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
+
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
